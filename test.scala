4b6e1f5f2bb60a6c5ebd921938882d2ce670a253
Kacper Korban <kacper.f.korban@gmail.com>
2022-04-01 02:23:26 +0200

    Add addTM test case

--- test.scala
+++ test.scala
@@ -17,12 +17,29 @@ class TMSuite extends munit.FunSuite {
     type incTM = TM[
       0,
       Action[0, "0", 0, "0", R] :+:
-        Action[0, "1", 1, "0", R] :+:
-          Action[1, "_", 2, "1", R] :+: Nl
+      Action[0, "1", 1, "0", R] :+:
+      Action[1, "_", 2, "1", R] :+:
+      Nl
     ]
     assertEquals(
       constValue[Eval[incTM, "01"]],
       "001"
     ) 
+  }
+
+  test("add") {
+    type addTM = TM[
+      0,
+      Action[0, "0", 0, "0", R] :+:
+      Action[0, "1", 1, "0", R] :+:
+      Action[1, "0", 1, "0", R] :+:
+      Action[1, "1", 2, "_", L] :+:
+      Action[2, "0", 3, "1", L] :+:
+      Nl
+    ]
+    assertEquals(
+      constValue[Eval[addTM, "01001"]],
+      "0001"
+    )
   }
 }
